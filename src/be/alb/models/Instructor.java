b75b1ee9b85e649ed13656c7d712f6b853186b67
GaryScript <gary.albrecq@condorcet.be>
2024-11-11 20:09:40 +0100

    add getters and setters in Person class

--- src/be/alb/models/Instructor.java
+++ src/be/alb/models/Instructor.java
@@ -1,57 +1,73 @@
 package be.alb.models;
+
 import java.time.LocalDate;
-import be.alb.dao.InstructorDAO;
 import java.util.ArrayList;
 import java.util.List;
+import be.alb.dao.InstructorDAO;
 
 public class Instructor extends Person {
-	private int id;
-	private List<Accreditation> accreditations;
-	private List<Lesson> lessons;
-	
-	public Instructor(int id, String name, String firstName, String city, String postalCode, String streetName, String streetNumber, LocalDate dob) {
+    private List<Accreditation> accreditations;
+    private List<Lesson> lessons;
+
+    public Instructor(int id, String name, String firstName, String city, String postalCode, String streetName, String streetNumber, LocalDate dob) {
         super(id, name, firstName, city, postalCode, streetName, streetNumber, dob);
-        this.accreditations = new ArrayList<>(); 
-        this.lessons = new ArrayList<>(); //
+        this.accreditations = new ArrayList<>();
+        this.lessons = new ArrayList<>();
     }
-	
-	public void AddLesson(Lesson lesson)
-	{
-		if(lesson == null)
-			throw new IllegalArgumentException();
-		
-		if(lessons.contains(lesson) == true)
-			throw new IllegalStateException();
-		
-		lessons.add(lesson);
-		
-//		boolean hasAccreditation = accreditations.stream()
-//		        .anyMatch(accreditation -> accreditation.matchesLessonType(lesson.getLessonType()));
-//		
-//		
-		// to do : verif accreditation, might be a seperated function actually
-		
-		// might need more verif
-		// might need a return ?
-	}
-	
-	public void RemoveLesson(Lesson lesson)
-	{
-		if(lesson == null)
-			throw new IllegalArgumentException();
-		
-		if(lessons.contains(lesson) == false)
-			throw new IllegalStateException();
-		
-		lessons.remove(lesson);
-		
-		// might need more verif
-		// might need a return ?
-	}
-	
-	public static List<Instructor> getAllInstructors() {
+
+    public void addLesson(Lesson lesson) {
+        if (lesson == null) {
+            throw new IllegalArgumentException("Lesson cannot be null");
+        }
+
+        if (lessons.contains(lesson)) {
+            throw new IllegalStateException("Instructor already teaches this lesson");
+        }
+
+//        if (!hasAccreditationForLesson(lesson)) {
+//            throw new IllegalStateException("This instructor hasn't the accreditation for this lesson");
+//        }
+
+        lessons.add(lesson);
+    }
+
+    // method to supress the lesson
+    public void removeLesson(Lesson lesson) {
+        if (lesson == null) {
+            throw new IllegalArgumentException("Lesson cannot be null.");
+        }
+
+        if (!lessons.contains(lesson)) {
+            throw new IllegalStateException("The instructor doesn't teach this lesson.");
+        }
+
+        lessons.remove(lesson);
+    }
+
+    // check if the instructor has the accreditation for this lesson 
+//    public boolean hasAccreditationForLesson(Lesson lesson) {
+//        return accreditations.stream()
+//                .anyMatch(accreditation -> accreditation.matchesLessonType(lesson.getLessonType()));
+//    }
+
+    // call the dao to get all instructors 
+    public static List<Instructor> getAllInstructors() {
         return InstructorDAO.getAllInstructors();
     }
-	
-	
+
+    public List<Accreditation> getAccreditations() {
+        return accreditations;
+    }
+
+    public void setAccreditations(List<Accreditation> accreditations) {
+        this.accreditations = accreditations;
+    }
+
+    public List<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public void setLessons(List<Lesson> lessons) {
+        this.lessons = lessons;
+    }
 }
