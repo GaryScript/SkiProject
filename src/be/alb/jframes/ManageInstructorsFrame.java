d4445f4f7745a26e12861f98927d8fb62e61c962
GaryScript <gary.albrecq@condorcet.be>
2024-11-11 19:33:33 +0100

    correct bad redirection from main frame to instructor frame

--- src/be/alb/jframes/ManageInstructorsFrame.java
+++ src/be/alb/jframes/ManageInstructorsFrame.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class ManageInstructors extends JFrame {
+public class ManageInstructorsFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -18,7 +18,7 @@ public class ManageInstructors extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ManageInstructors frame = new ManageInstructors();
+					ManageInstructorsFrame frame = new ManageInstructorsFrame();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,7 +30,7 @@ public class ManageInstructors extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public ManageInstructors() {
+	public ManageInstructorsFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 913, 878);
 		contentPane = new JPanel();
