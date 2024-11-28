e7a32d6cec4b617314617c80521c1f09e5273722
GaryScript <gary.albrecq@condorcet.be>
2024-11-28 11:30:50 +0100

    modified period

--- src/be/alb/models/Period.java
+++ src/be/alb/models/Period.java
@@ -1,5 +1,57 @@
 package be.alb.models;
 
+import java.time.LocalDate;
+
 public class Period {
 
+    private int periodId; 
+    private LocalDate startDate; 
+    private LocalDate endDate;   
+    private boolean isVacation; 
+
+    public Period(LocalDate startDate, LocalDate endDate, boolean isVacation) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isVacation = isVacation;
+    }
+
+    public Period(int periodId, LocalDate startDate, LocalDate endDate, boolean isVacation) {
+        this.periodId = periodId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isVacation = isVacation;
+    }
+
+    public int getPeriodId() {
+        return periodId;
+    }
+
+    public void setPeriodId(int periodId) {
+        this.periodId = periodId;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isVacation() {
+        return isVacation;
+    }
+
+    public void setVacation(boolean vacation) {
+        isVacation = vacation;
+    }
 }
+
